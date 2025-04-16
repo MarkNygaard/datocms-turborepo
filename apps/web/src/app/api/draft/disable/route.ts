a4a68ed092ae28fe7db3f095ed01891c0989bae1
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-04-16 21:58:15 +0200

    draft disable fixed

--- apps/web/src/app/api/draft/disable/route.ts
+++ apps/web/src/app/api/draft/disable/route.ts
@@ -1,26 +1,37 @@
+import type { NextRequest } from "next/server";
 import { cookies, draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
-  const url = searchParams.get("url") ?? searchParams.get("token");
+  const token =
+    request.headers.get("x-preview-token") ?? searchParams.get("token");
+  const url = searchParams.get("url");
 
-  (await draftMode()).disable();
+  if (token !== process.env.DRAFT_SECRET_TOKEN) {
+    return new Response("Invalid token", { status: 401 });
+  }
 
-  if (!url) return new Response("Draft mode is disabled");
+  (await draftMode()).enable();
 
-  //to avoid losing the cookie on redirect in the iFrame
+  if (!url) return new Response("Draft mode is enabled");
+
+  // Retain draftMode cookie in iframe context
   const cookieStore = await cookies();
-  const cookie = cookieStore.get("__prerender_bypass")!;
-  cookieStore.set({
-    name: "__prerender_bypass",
-    value: cookie?.value,
-    httpOnly: true,
-    path: "/",
-    secure: true,
-    sameSite: "none",
-  });
+  const cookie = cookieStore.get("__prerender_bypass");
+
+  if (cookie) {
+    cookieStore.set({
+      name: "__prerender_bypass",
+      value: cookie.value,
+      httpOnly: true,
+      path: "/",
+      secure: true,
+      sameSite: "none",
+      partitioned: true,
+    });
+  }
 
   redirect(url);
 }
