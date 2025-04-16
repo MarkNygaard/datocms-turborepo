b5f67e522b50fd72be68a21f90df25362b78eeb6
Mark Nygaard Leth <mark.nygaard@hotmail.com>
2025-04-16 22:11:55 +0200

    more draft work

--- apps/web/src/app/api/draft/disable/route.ts
+++ apps/web/src/app/api/draft/disable/route.ts
@@ -1,25 +1,18 @@
-import type { NextRequest } from "next/server";
 import { cookies, draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
-  const token =
-    request.headers.get("x-preview-token") ?? searchParams.get("token");
   const url = searchParams.get("url");
 
-  if (token !== process.env.DRAFT_SECRET_TOKEN) {
-    return new Response("Invalid token", { status: 401 });
-  }
+  (await draftMode()).disable();
 
-  (await draftMode()).enable();
-
-  if (!url) return new Response("Draft mode is enabled");
+  if (!url) return new Response("Draft mode is disabled");
 
   // Retain draftMode cookie in iframe context
   const cookieStore = await cookies();
-  const cookie = cookieStore.get("__prerender_bypass");
+  const cookie = cookieStore.get("__prerender_bypass")!;
 
   if (cookie) {
     cookieStore.set({
--- apps/web/src/app/api/draft/enable/route.ts
+++ apps/web/src/app/api/draft/enable/route.ts
@@ -5,12 +5,11 @@ import { redirect } from "next/navigation";
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
-  const token = request.headers.get("x-preview-token");
+  const token = searchParams.get("token");
   const url = searchParams.get("url");
 
-  if (token !== process.env.DRAFT_SECRET_TOKEN) {
+  if (token !== process.env.DRAFT_SECRET_TOKEN)
     return new Response("Invalid token", { status: 401 });
-  }
 
   (await draftMode()).enable();
 
@@ -18,7 +17,7 @@ export async function GET(request: NextRequest) {
 
   // Retain draftMode cookie in iframe context
   const cookieStore = await cookies();
-  const cookie = cookieStore.get("__prerender_bypass");
+  const cookie = cookieStore.get("__prerender_bypass")!;
 
   if (cookie) {
     cookieStore.set({
